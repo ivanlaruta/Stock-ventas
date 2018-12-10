e388e860cc386b6f47ce7d449515804b830e7b4f
BOLIVIA\ivan.laruta <ivan.laruta@toyosa.com>
2018-12-10 19:49:38 -0400

    mas cambios

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -77,9 +77,9 @@
                       <li><a  href="{{ route('notas.dashboard',['v_aux'=>'0','f_ini'=>'0','f_fin'=>'0','title'=>'index','mes'=>'0','regional'=>'0','marca'=>'0','sucursal'=>'0','modelo'=>'0'])}}"> Notas de entrega</a></li> 
                       
                        {{-- <li><a href="{{ route('metas.index',['periodo'=>'0','marca'=>'0','regional'=>'0','sucursal'=>'0'])}}">Metas</a></li> --}}
-                        @if(Auth::user()->rol<>'11')
+                        
                        <li><a  href="{{ route('seguimiento.index')}}"> Seguimineto de unidades</a></li>
-                       @endif
+                      
                     </ul>
                   </li>
 
